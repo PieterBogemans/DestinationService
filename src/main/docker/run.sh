90a30309a14fc75fee6d0bf4572f60198d00e043
Pieter Bogemans <pieterbogemans@hotmail.com>
2017-12-13 18:09:33 +0100

    Attempt 20

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/destinationservice/@project.build.finalName@.jar
